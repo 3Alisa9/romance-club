12c1c5bbf99b61c1d272f06889c21f6bea134c13
3Alisa9 <varlacheva.aliska@gmail.com>
2026-08-29 13:49:43 +0000

    Sat, Aug 29, 2026, 8:49 PM +07:00

--- index.py
+++ index.py
@@ -21,9 +21,7 @@ for filename in os.listdir(image_folder):
         inputs = processor(images=image, return_tensors="pt")
         with torch.no_grad():
             outputs = model.get_image_features(**inputs.to(device))
-if hasattr(outputs, 'pooler_output'):
-        outputs = model.get_image_features(**inputs.to(device))
-if hasattr(outputs, 'pooler_output'):
+            emb = outputs.pooler_output.detach().cpu().numpy().astype(np.float32)
         embeddings.append(emb)
         name = os.path.splitext(filename)[0]
         names.append(name)
